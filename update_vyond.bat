715bd09b13c50285f2e167a58fa96bb959d2a08d
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-19 17:52:22 -0400

    Update update_vyond.bat

--- update_vyond.bat
+++ update_vyond.bat
@@ -2,5 +2,17 @@
 title Vyond Legacy Offline [Updating...]
 echo Updating....
 git pull
+pushd wrapper
+if exist config-offline.json (
+if exist config-online.json (
+del config-online.json
+)
+)
+if exist env-nodebug.json (
+if exist env-debug.json (
+del env-debug.json
+)
+)
+pushd ..\
 echo Vyond Legacy Offline has been updated!
 pause

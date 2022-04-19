ee34bc6046b9c09c2cc0ea40a78f72914c12e1ab
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-19 17:59:17 -0400

    Update update_vyond.bat

--- update_vyond.bat
+++ update_vyond.bat
@@ -2,6 +2,7 @@
 title Vyond Legacy Offline [Updating...]
 echo Updating....
 git pull
+:: Delete any files added when the online lvm feature and debug mode is turned on.
 pushd wrapper
 if exist config-offline.json (
 if exist config-online.json (

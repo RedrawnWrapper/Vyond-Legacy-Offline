a56aad24d20a0e45058fdbece4660dd82b13c184
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-22 11:08:01 -0400

    Update pagelvp.js

--- wrapper/static/pagelvp.js
+++ wrapper/static/pagelvp.js
@@ -17,14 +17,13 @@ module.exports = function (req, res, url) {
 		default:
 			return;
 	}
-  if (process.env.OFFLINE_SERVER == "Y") {
+	if (process.env.OFFLINE_SERVER == "Y") {
 		redirectUrl = `https://localhost:8043/player?movieId=${params.flashvars.movieId}`;
 	} else {
 		server = "https://josephanimate2021.github.io";
 		redirectUrl = `${server}/lvm-static/offline-player?movieId=${params.flashvars.movieId}`;
 	}
 	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-	Object.assign(params.flashvars, query);
 	res.end(`<html><head><script>function Redirect(){window.location="${redirectUrl}";}</script><body onload="Redirect()"></body></html>`);
 	return true;
 }

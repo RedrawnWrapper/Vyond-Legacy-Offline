0bbfb6e2d8b330b6193aeb244f1483e8269a3b43
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-22 18:42:20 -0400

    Update update_vyond.bat

--- update_vyond.bat
+++ update_vyond.bat
@@ -1,6 +1,12 @@
 @echo off
 title Vyond Legacy Offline [Updating...]
 echo Updating....
+:: Save the config in a temp copy during the update.
+pushd utilities
+if exist config.bat (
+ren config.bat tempconfig.bat
+)
+pushd ..\
 git pull
 :: Delete any files added when the online lvm feature and debug mode is turned on.
 pushd wrapper
@@ -18,5 +24,14 @@ pushd ..\
 if exist 405-error-redirect-fix.js (
 del 405-error-redirect-fix.js
 )
+:: Rename the temp copy of the config.bat file to the main copy of the config.bat file after the update.
+pushd utilities
+if exist tempconfig.bat (
+if exist config.bat (
+del config.bat
+)
+ren tempconfig.bat config.bat
+)
+pushd ..\
 echo Vyond Legacy Offline has been updated!
 pause

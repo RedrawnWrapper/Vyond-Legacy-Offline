ff1dc0b8fbf199fa338b5af04a0a1701fbca72f9
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-22 12:37:45 -0400

    Update pagelvp.js

--- wrapper/static/pagelvp.js
+++ wrapper/static/pagelvp.js
@@ -1,21 +1,3 @@
-const fUtil = require('../fileUtil');
-const stuff = require('./info');
-
-function toAttrString(table) {
-	return typeof (table) == 'object' ? Object.keys(table).filter(key => table[key] !== null).map(key =>
-		`${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`).join('&') : table.replace(/"/g, "\\\"");
-}
-function toParamString(table) {
-	return Object.keys(table).map(key =>
-		`<param name="${key}" value="${toAttrString(table[key])}">`
-	).join(' ');
-}
-function toObjectString(attrs, params) {
-	return `<object id="obj" ${Object.keys(attrs).map(key =>
-		`${key}="${attrs[key].replace(/"/g, "\\\"")}"`
-	).join(' ')}>${toParamString(params)}</object>`;
-}
-
 module.exports = function (req, res, url) {
 	if (req.method != 'GET') return;
 	const query = url.query;
@@ -42,6 +24,7 @@ module.exports = function (req, res, url) {
 		redirectUrl = `${server}/lvm-static/offline-player?movieId=${params.flashvars.movieId}`;
 	}
 	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+	Object.assign(params.flashvars, query);
 	res.end(`<html><head><script>function Redirect(){window.location="${redirectUrl}";}</script><body onload="Redirect()"></body></html>`);
 	return true;
 }

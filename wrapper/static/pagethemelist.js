b761fdd7abf86ee1643a0c0ec95e815115572455
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-23 05:59:06 -0400

    Create pagethemelist.js

---
+++ wrapper/static/pagethemelist.js
@@ -0,0 +1,59 @@
+function toAttrString(table) {
+	return typeof table == "object"
+		? Object.keys(table)
+				.filter((key) => table[key] !== null)
+				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`)
+				.join("&")
+		: table.replace(/"/g, '\\"');
+}
+function toParamString(table) {
+	return Object.keys(table)
+		.map((key) => `<param name="${key}" value="${toAttrString(table[key])}">`)
+		.join(" ");
+}
+function toObjectString(attrs, params) {
+	return `<object ${Object.keys(attrs)
+		.map((key) => `${key}="${attrs[key].replace(/"/g, '\\"')}"`)
+		.join(" ")}>${toParamString(params)}</object>`;
+}
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
+module.exports = function (req, res, url) {
+	if (req.method != "GET") return;
+	const query = url.query;
+
+	var params, returnUrl, playerPath;
+	switch (url.pathname) {
+		case "/videomaker": {
+			break;
+		}
+
+		default:
+			return;
+	}
+	if (process.env.OFFLINE_SERVER == "Y") {
+		returnUrl = "https://localhost:8043";
+		playerPath = "themeChooser";
+	} else {
+		returnUrl = "https://josephanimate2021.github.io";
+		playerPath = "lvm-static/themeChooser?return=http://localhost:4343/";
+	}
+	res.setHeader("Content-Type", "text/html; charset=UTF-8");
+	Object.assign(params.flashvars, query);
+	res.end(`<html>
+	<head>
+		<script>
+			function genorateId() { 
+				window.location = '${returnUrl}/${playerPath}'; 
+			}
+		</script>
+	</head>
+	<body onload="genorateId()"></body>
+</html>`)
+	return true;
+};

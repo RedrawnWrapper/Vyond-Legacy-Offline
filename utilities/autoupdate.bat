b4ca18091f14c4b704fd2618b80ea99ae75fedc5
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-01 14:12:06 -0400

    Bug fixes

--- utilities/autoupdate.bat
+++ utilities/autoupdate.bat
@@ -2,11 +2,9 @@
 title Vyond Legacy Offline [Updating...]
 echo Updating....
 :: Save the config in a temp copy before the update.
-cd %USERPROFILE%\Downloads\Vyond-Legacy-Offline\utilities
 if exist config.bat (
 ren config.bat tempconfig.bat
 )
-cd %USERPROFILE%\Downloads\Vyond-Legacy-Offline
 call PortableGit\bin\git.exe pull
 :: Delete any files added when the online lvm feature and debug mode is turned on.
 pushd wrapper

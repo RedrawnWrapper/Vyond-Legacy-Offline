19616927b5c5d43064c71df24fb919da2ae71144
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-01 12:20:25 -0400

    Create install_wrapper_offline_portable_installer.bat

---
+++ install_wrapper_offline_portable_installer.bat
@@ -0,0 +1,6 @@
+@echo off
+title Vyond Legacy Offline [Installing The Portable Installer Of Wrapper: Offline...]
+echo Installing The Wrapper: Offline Portable Installer. This Should Take A Few Minutes...
+call utilities/PortableGit/bin/git.exe clone https://github.com/josephanimate2021/Wrapper-Offline-Portable-Installer.git
+echo The Portable Installer Of Wrapper: Offline Has Been Installed.
+pause

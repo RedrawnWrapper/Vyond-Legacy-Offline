cfa41e60f3213b7477a029c77ef95fae9835a02d
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-06 18:11:03 -0400

    Update list.js

--- wrapper/asset/list.js
+++ wrapper/asset/list.js
@@ -65,6 +65,7 @@ module.exports = function (req, res, url) {
 		case '/api_v2/assets/shared': { makeZip = true; break; }
 		case '/goapi/getUserAssetsXml/': break;
 		case '/goapi/deleteUserTemplate/': { starter.delete(); break; }
+		case '/goapi/updateSysTemplateAttributes/': { starter.update(); break; }
 		default: return;
 	}
 

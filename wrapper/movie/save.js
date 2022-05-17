0cb6b114358b49f07b5ac82d69cb39f2785da240
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-17 12:17:00 -0400

    Update save.js

--- wrapper/movie/save.js
+++ wrapper/movie/save.js
@@ -8,11 +8,10 @@ module.exports = function (req, res, url) {
 		return true;
 	}
 	const trigAutosave = req.body.is_triggered_by_autosave;
-		if (trigAutosave && !req.body.movieId) return res.end('0');
+	if (trigAutosave && !req.body.movieId) return res.end('0');
 
-		var body = Buffer.from(req.body.body_zip, 'base64');
-		var thumb = trigAutosave ? null : Buffer.from(req.body.thumbnail_large, 'base64');
-		movie.save(body, thumb, req.body.movieId || req.body.presaveId, req.body.presaveId).then(nId => res.end('0' + nId));
-	};
+	var body = Buffer.from(req.body.body_zip, 'base64');
+	var thumb = trigAutosave ? null : Buffer.from(req.body.thumbnail_large, 'base64');
+	movie.save(body, thumb, req.body.movieId || req.body.presaveId, req.body.presaveId).then(nId => res.end('0' + nId));
 	return true;
 }

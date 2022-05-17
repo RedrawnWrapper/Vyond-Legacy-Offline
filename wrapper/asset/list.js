c47d8dd4337a512235755b44681043cc97c0db19
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-17 13:04:55 -0400

    Update list.js

--- wrapper/asset/list.js
+++ wrapper/asset/list.js
@@ -64,7 +64,7 @@ module.exports = function (req, res, url) {
 		case '/api_v2/assets/team':
 		case '/api_v2/assets/shared': { makeZip = true; break; }
 		case '/goapi/getUserAssetsXml/': break;
-		// testing the starter/delete.js file. case '/goapi/deleteUserTemplate/': { starter.delete(); break; }
+		case '/goapi/deleteUserTemplate/': { starter.delete(); break; }
 		case '/goapi/updateSysTemplateAttributes/': { starter.update(); break; }
 		default: return;
 	}

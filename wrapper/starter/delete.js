b3211b4cc260495ddd932c8f819b6ff7d07af898
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-17 13:00:32 -0400

    Update delete.js

--- wrapper/starter/delete.js
+++ wrapper/starter/delete.js
@@ -3,10 +3,10 @@ const starter = require('./main');
 module.exports = function (req, res, url) {
 	switch (url.path) {
 		case '/goapi/deleteUserTemplate/': { 
-      starter.delete(); 
-      starter.deleteThumb();
-      break; 
-    }
-    default: return;
-  }
+			starter.delete(); 
+			starter.deleteThumb();
+			break; 
+		}
+		default: return;
+	}
 }

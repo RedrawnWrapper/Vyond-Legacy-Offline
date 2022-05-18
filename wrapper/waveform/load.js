65768a511fcdd139098242167c9c01fa0e5a3944
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-17 22:23:05 -0400

    Update load.js

--- wrapper/waveform/load.js
+++ wrapper/waveform/load.js
@@ -1,11 +1,20 @@
-const waveform = require('./main');
+const loadPost = require("../request/post_body");
+const asset = require("../asset/main");
+const http = require("http");
 
-module.exports = function (url) {
-	switch (url.path) {
-		case '/goapi/getWaveForm/': { 
-			waveform.load(); 
-			break; 
-		}
-		default: return;
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
+module.exports = function (req, res, url) {
+	if (req.method != "POST") return;
+	switch (url.pathname) {
+		case "/goapi/getWaveForm/":
+			loadPost(req, res).then(([data]) => {
+				asset.loadLocal(data.presaveId, '-waveform.wf');
+			});
+			return true;
 	}
-}
+};

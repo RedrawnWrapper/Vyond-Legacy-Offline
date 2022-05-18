379e1ddd68e87dee0188e13a5d4cb83c862b0f27
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-17 22:15:39 -0400

    Update save.js

--- wrapper/waveform/save.js
+++ wrapper/waveform/save.js
@@ -1,24 +1,21 @@
-const loadPost = require('../request/post_body');
-const waveform = require('./main');
+const loadPost = require("../request/post_body");
+const asset = require("./main");
+const http = require("http");
 
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
 module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/goapi/saveWaveform/') return;
-	loadPost(req, res).then(data => {
-		
-		if (!data.body_zip) {
-			res.statusCode = 400;
-			res.end();
+	if (req.method != "POST") return;
+	switch (url.pathname) {
+		case "/goapi/saveWaveForm/":
+			loadPost(req, res).then(([data, wId]) => {
+				var bytes = Buffer.from(data.bytes, "base64");
+				asset.save(bytes, wId, "waveform", "wf");
+			});
 			return true;
-		}
-
-		const body = Buffer.from(data.body_zip, 'base64');
-		
-		try {
-			const wId = waveform.save(body);
-			res.end('0' + wId);
-		} catch (e) {
-			console.error(e);
-		}
-	});
-	return true;
-}
+	}
+};

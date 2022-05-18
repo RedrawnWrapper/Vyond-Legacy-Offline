fb5fdfa11dbe5e55f485ed8bc44a8c92d85a548f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-05-17 22:00:18 -0400

    Update main.js

--- wrapper/waveform/main.js
+++ wrapper/waveform/main.js
@@ -1,17 +1,28 @@
-const fs = require("fs");
-const folder = "../_WAVEFORMS";
+const parse = require('../data/parse');
+const fUtil = require('../fileUtil');
+const nodezip = require('node-zip');
+const fs = require('fs');
 
 module.exports = {
-	load(aId) {
-		var match = false;
-		fs.readdirSync(`${folder}`)
-			.forEach(filename => {
-				if (filename.search(aId) !== -1) match = filename;
-			})
-		return match ? fs.readFileSync(`${folder}/${match}`) : null;
+	/**
+	 *
+	 * @param {Buffer} movieZip
+	 * @param {string} nëwId
+	 * @param {string} oldId
+	 * @returns {Promise<string>}
+	 */
+	save(waveZip) {
+		return new Promise((res, rej) => {
+			const zip = nodezip.unzip(waveZip);
+			var wId = fUtil.getNextFileId('waveform-', '.wf');
+			let path = fUtil.getFileIndex('waveform-', '.wf', wId);
+			let writeStream = fs.createWriteStream(path);
+			parse.unpackZip(zip).then(data => {
+				writeStream.write(data, () => {
+					writeStream.close();
+					res('0-' + wId);
+				});
+			});
+                });
 	},
-	save(wf, aId) {
-		fs.writeFileSync(`${folder}/${aId}.wf`, wf);
-		return true;
-	}
-};
+}
